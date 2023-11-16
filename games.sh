08bf3e3cda5f8a40aa0642d7a7969f13c45d5b5c
Joni <github01@pesonen.it>
2023-11-16 08:25:19 +0200

    first commit

---
+++ games.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+allgames=(nudoku nsnake moon-buggy)
+
+lenght=${allgames[@]}
+
+for t in $lenght; do
+  if ! command -v $t &> /dev/null
+  then
+    apt install $t
+  fi
+done
+
+random=$(($RANDOM % ${#allgames[@]}))
+
+${allgames[$random]}
